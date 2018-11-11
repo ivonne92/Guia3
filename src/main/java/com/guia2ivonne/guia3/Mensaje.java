5dfe33307b4aa9f624165aee4328bef5765827f3
ivonne92 <43704226+ivonne92@users.noreply.github.com>
2018-11-11 15:32:26 -0600

    Add files via upload

---
+++ src/main/java/com/guia2ivonne/guia3/Mensaje.java
@@ -0,0 +1,11 @@
+package com.guia2ivonne.guia3;
+
+public class Mensaje {
+    public String mensaje;
+    public String hora;
+
+    public Mensaje(String mensaje, String hora) {
+        this.mensaje = mensaje;
+        this.hora = hora;
+    }
+}
